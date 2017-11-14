f48199467511f29b8b1324a9129a81e01a830191
Willy <One19wil@gmail.com>
2017-11-14 21:14:11 +0100

    Ajout foret de point
    Test performance

--- Source/Game/GameInstance.cpp
+++ Source/Game/GameInstance.cpp
@@ -1,22 +1,39 @@
 #include "GameInstance.h"
 
 
+#include <cstdlib>
+#include <string>
+#include <vector>
+
 void game(Engine *pEngine)
 {
 	Rendering* render = pEngine->getRenderModule();
 
+	ConfigLoader* loader = pEngine->getConfig();
+
 	// Affiche un cercle
+	
+	std::vector<sf::CircleShape> shape(10000);
 
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
-	shape.setPosition(300, 300);
+	for(int i=0;i<10000;i++)
+	{
+		shape[i].setRadius(5);
+		shape[i].setPosition(float(rand() % loader->screenSize.x), float(rand() % loader->screenSize.y));
+		shape[i].setFillColor(sf::Color::Blue);
 
-	render->addDrawingObject("rond", &shape);
+		render->addDrawingObject(std::to_string(i), &shape[i]);
+	}
 
 
 	while (pEngine->isOpen())
 	{
-		Sleep(1000);
+		Sleep(6);
+
+		for (int i = 0; i<10000; i++)
+		{
+			shape[i].move(float(rand() % 2), float(rand() % 2));
+		}
+
 	}
 
 }
